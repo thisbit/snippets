c47cf37c0cff07a3757232ef6d9c38c3e00d189c
thisbit <elvis.krstulovic@fokusgrupa.net>
2023-02-27 01:30:05 +0100

    taylor drayson custom functions elements

--- wp/generatepress/custom-functions.php
+++ wp/generatepress/custom-functions.php
@@ -7,6 +7,7 @@
  *
  * Replace $key with your unique prefix for all custom functions
  * here is the original snippet https://gist.github.com/tdrayson/391573513bf54a6b2ab6ee2d57d7fadb
+ * usecase https://share.getcloudapp.com/xQuxbn1o?fbclid=IwAR20y6OYDppFIUXwYckQa5o9L8kkl2081g8ss0Nij179bmu0qvHkxiPk9PQ
 */
 
 add_filter( 'generateblocks_dynamic_content_output', function( $content, $attributes ) {
