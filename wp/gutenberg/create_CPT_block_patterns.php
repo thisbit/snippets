be6ec991f701138554ba1a5b647929a4c0d06b12
thisbit <53581066+thisbit@users.noreply.github.com>
2023-03-19 16:08:59 +0100

    removed unrelated function

--- wp/gutenberg/create_CPT_block_patterns.php
+++ wp/gutenberg/create_CPT_block_patterns.php
@@ -137,21 +137,3 @@ if( ! function_exists( 'unregister_post_type' ) ) {
 }
 
 // add_action('init','unregister_post_type'); // run this if you intend to stop using the plugin
-
-
-
-// List all registered post types 
-function apuri_debug_list_post_types() {
-	$post_types = get_post_types( '', 'names' ); 
- 
-	echo '<pre style="min-height: 720px;">';
-	echo '<h4>Registered Post Types</h4>';
-			
-	foreach ( $post_types as $post_type ) {
-		echo $post_type . '<br>' ; 
-	}
-	
-	echo '</pre>';
-}
-
-add_action( 'wp_head', 'apuri_debug_list_post_types' );
