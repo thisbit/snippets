37a622ff2117e728fcf067b694b146ac481107f4
thisbit <53581066+thisbit@users.noreply.github.com>
2023-11-12 13:35:22 +0100

    Create media-categories.php

---
+++ wp/media/media-categories.php
@@ -0,0 +1,8 @@
+<?php
+// Adds categories to wp media
+if ( ! function_exists('apuri_add_categories_to_attachments') ) {
+	function thisbit_add_categories_to_attachments() {
+			register_taxonomy_for_object_type( 'category', 'attachment' );
+	} 
+	add_action( 'init' , 'thisbit_add_categories_to_attachments' );
+}
