3d0bec362b25a42eb28f191f23bae0e01aa5ea75
thisbit <53581066+thisbit@users.noreply.github.com>
2023-11-12 13:37:26 +0100

    Update media-categories.php

--- wp/media/media-categories.php
+++ wp/media/media-categories.php
@@ -1,6 +1,6 @@
 <?php
 // Adds categories to wp media
-if ( ! function_exists('apuri_add_categories_to_attachments') ) {
+if ( ! function_exists('thisbit_add_categories_to_attachments') ) {
 	function thisbit_add_categories_to_attachments() {
 			register_taxonomy_for_object_type( 'category', 'attachment' );
 	} 
