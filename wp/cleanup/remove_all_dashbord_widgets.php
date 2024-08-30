7b61611d0ed42ba7af162e2a004c34f000f103de
thisbit <elvis.krstulovic@fokusgrupa.net>
2024-08-30 12:01:19 +0200

    added function

--- wp/cleanup/remove_all_dashbord_widgets.php
+++ wp/cleanup/remove_all_dashbord_widgets.php
@@ -1,1 +1,12 @@
-remove_all_dashbord_widgets.php
+<?php
+/**
+ * We set the $wp_meta_boxes array to empty when on dashboard, this will hide any and all dashboard items by core or 3rd party
+ **/
+
+function thisbit_remove_all_dashboard_widgets() {
+	global $wp_meta_boxes;
+
+	$wp_meta_boxes['dashboard'] = array();
+}
+
+add_action('wp_dashboard_setup', 'thisbit_remove_all_dashboard_widgets', 999 );
